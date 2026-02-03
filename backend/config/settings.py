3cba1a891fcfadbad133a5a8b50c86155a8bcf8c
EC2 Default User <ec2-user@ip-172-31-12-90.ap-south-1.compute.internal>
2026-02-03 12:48:07 +0000

    Settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,7 +29,7 @@ load_dotenv(dotenv_path=env_path)
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = False
 
 if DEBUG:
     ALLOWED_HOSTS = ['*']
